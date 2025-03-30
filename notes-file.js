a8246f14c6f011854cd724da7b95645aaeace020
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-03-30 00:48:14 -0400

    initial commit.

---
+++ notes-file.js
@@ -0,0 +1,15 @@
+// Intermediate Javascript
+
+// Random Number Generation
+// generates a random number between 0 & 0.99999999999
+let n = Math.random();
+// n will be a different number every time this code is run
+console.log(`n = ${n}`);
+
+// multiplies n by 6
+n = n * 6;
+console.log(`Multiplied n by 6. n = ${n}`);
+
+// round n down to the nearest whole number 
+n = Math.floor(n);
+console.log(`Rounded n down to the nearest whole number\nn = ${n}`);
